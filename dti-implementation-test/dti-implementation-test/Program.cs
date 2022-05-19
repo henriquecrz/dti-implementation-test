6deeff2f07813ea06d47c1001f1fc39676f0e24c
Henrique Andrade <henrique.andrade22@hotmail.com>
2022-05-18 21:58:26 -0300

    Refactoring

--- dti-implementation-test/dti-implementation-test/Program.cs
+++ dti-implementation-test/dti-implementation-test/Program.cs
@@ -7,18 +7,46 @@ namespace dti_implementation_test
 {
     public class Program
     {
-        static void Main(string[] args)
+        private const int EXPECTED_PARAMETERS_NUMBER = 3;
+        private const int DATE_ARG_INDEX = 0;
+        private const int SMALL_DOG_ARG_INDEX = 1;
+        private const int LARGE_DOG_ARG_INDEX = 2;
+
+        public static void Main(string[] args)
         {
-            Input input = GetInput();
-            List<IPetShop> petShops = GetPetShops(input);
-            Catalog catalog = new Catalog();
+            Input input;
+
+            try
+            {
+                input = CreateInput(args);
+            }
+            catch (Exception)
+            {
+                input = CreateInput();
+            }
+
+            var petShops = GetPetShops(input);
+            var catalog = new Catalog();
 
             FillCatalog(catalog, petShops);
             ShowBestPetShop(catalog);
             Console.ReadKey();
         }
 
-        static Input GetInput()
+        private static Input CreateInput(string[] args)
+        {
+            if (args?.Length < EXPECTED_PARAMETERS_NUMBER ||
+                !DateTime.TryParse(args[DATE_ARG_INDEX], out var date) ||
+                !int.TryParse(args[SMALL_DOG_ARG_INDEX], out var smallDogs) ||
+                !int.TryParse(args[LARGE_DOG_ARG_INDEX], out var largeDogs))
+            {
+                throw new Exception();
+            }
+
+            return new Input(date, smallDogs, largeDogs);
+        }
+
+        private static Input CreateInput()
         {
             bool isParseSuccess;
             DateTime date;
@@ -65,11 +93,11 @@ namespace dti_implementation_test
             return new Input(date, smallDogs, largeDogs);
         }
 
-        static List<IPetShop> GetPetShops(Input input)
+        private static List<IPetShop> GetPetShops(Input input)
         {
-            MeuCaninoFeliz meuCaninoFeliz = new MeuCaninoFeliz("Meu Canino Felix", 2000, input);
-            VaiRex vaiRex = new VaiRex("Vai Rex", 1700, input);
-            ChowChawgas chowChawgas = new ChowChawgas("Chow Chawgas", 800, input);
+            var meuCaninoFeliz = new MeuCaninoFeliz("Meu Canino Felix", 2000, input);
+            var vaiRex = new VaiRex("Vai Rex", 1700, input);
+            var chowChawgas = new ChowChawgas("Chow Chawgas", 800, input);
 
             return new List<IPetShop>
             {
@@ -79,15 +107,15 @@ namespace dti_implementation_test
             };
         }
 
-        static void FillCatalog(Catalog catalog, List<IPetShop> petShops) => petShops.ForEach(petShop => catalog.Add(petShop));
+        private static void FillCatalog(Catalog catalog, List<IPetShop> petShops) =>
+            petShops.ForEach(petShop => catalog.Add(petShop));
 
-        static void ShowBestPetShop(Catalog catalog)
+        private static void ShowBestPetShop(Catalog catalog)
         {
-            IPetShop bestPetShop = catalog.GetBestPetShop();
-
-            string message = (bestPetShop is null)
-                ? Message.NO_REGISTERED_PET_SHOPS
-                : $"\nThe better pet shop choice is {bestPetShop.Name}.\nThe better price is {bestPetShop.Amount:C}.";
+            var bestPetShop = catalog.GetBestPetShop();
+            var message = bestPetShop is null ?
+                Message.NO_REGISTERED_PET_SHOPS :
+                $"\nThe better pet shop choice is {bestPetShop.Name}.\nThe better price is {bestPetShop.Amount:C}.";
 
             Console.WriteLine(message);
         }
